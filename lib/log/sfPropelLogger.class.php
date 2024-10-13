7e61aebedcd63ef0df04c0aae432d5db30fb1971
Sébastien Coueille <32077930+scoueille@users.noreply.github.com>
2024-10-13 19:50:23 +0200

    update Logger

--- lib/log/sfPropelLogger.class.php
+++ lib/log/sfPropelLogger.class.php
@@ -1,5 +1,8 @@
 <?php
 
+use Psr\Log\AbstractLogger;
+use Psr\Log\LoggerInterface;
+
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
@@ -16,7 +19,7 @@
  * @author     Dustin Whittle <dustin.whittle@symfony-project.com>
  * @version    SVN: $Id: sfPropelLogger.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class sfPropelLogger implements BasicLogger
+class sfPropelLogger extends AbstractLogger
 {
   protected
     $dispatcher = null;
@@ -38,84 +41,17 @@ class sfPropelLogger implements BasicLogger
     }
   }
 
-  /**
-   * A convenience function for logging an alert event.
-   *
-   * @param mixed $message the message to log.
-   */
-  public function alert($message)
-  {
-    $this->log($message, sfLogger::ALERT);
-  }
-
-  /**
-   * A convenience function for logging a critical event.
-   *
-   * @param mixed $message the message to log.
-   */
-  public function crit($message)
-  {
-    $this->log($message, sfLogger::CRIT);
-  }
-
-  /**
-   * A convenience function for logging an error event.
-   *
-   * @param mixed $message the message to log.
-   */
-  public function err($message)
-  {
-    $this->log($message, sfLogger::ERR);
-  }
-
-  /**
-   * A convenience function for logging a warning event.
-   *
-   * @param mixed $message the message to log.
-   */
-  public function warning($message)
-  {
-    $this->log($message, sfLogger::WARNING);
-  }
-
-  /**
-   * A convenience function for logging an critical event.
-   *
-   * @param mixed $message the message to log.
-   */
-  public function notice($message)
-  {
-    $this->log($message, sfLogger::NOTICE);
-  }
-
-  /**
-   * A convenience function for logging an critical event.
-   *
-   * @param mixed $message the message to log.
-   */
-  public function info($message)
-  {
-    $this->log($message, sfLogger::INFO);
-  }
-
-  /**
-   * A convenience function for logging a debug event.
-   *
-   * @param mixed $message the message to log.
-   */
-  public function debug($message)
-  {
-    $this->log($message, sfLogger::DEBUG);
-  }
-
-  /**
-   * Primary method to handle logging.
-   *
-   * @param mixed $message the message to log.
-   * @param int $severity The numeric severity. Defaults to null so that no assumptions are made about the logging backend.
-   */
-  public function log($message, $severity = sfLogger::DEBUG)
-  {
-    $this->dispatcher->notify(new sfEvent($this, 'application.log', array($message, 'priority' => $severity)));
-  }
+    /**
+     * Primary method to handle logging.
+     *
+     * @param $level
+     * @param $message the message to log.
+     * @param array $context
+     * @return void
+     *
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $this->dispatcher->notify(new sfEvent($this, 'application.log', array($message, 'priority' => $level)));
+    }
 }
